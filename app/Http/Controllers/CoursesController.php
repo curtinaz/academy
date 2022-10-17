53931c0ed6abb2f41e96d6416d3239b7dbec77ea
Cristian Curtinaz <curtinazif@gmail.com>
2022-10-17 07:52:25 -0300

    createCourse added

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateCourseRequest;
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CoursesController extends Controller
 {
     public function getCourses() {
-        return [
-            "name" => "Laravel 9 Curso Completo",
-            "alunos" => 75
-        ];
+        $courses = Course::all();
+        return $courses;
+    }
+
+    public function createCourse(CreateCourseRequest $req) {
+        $course = Course::create([
+            "name" => $req->name,
+            "description" => $req->description 
+        ]);
+
+        return $course;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/courses',[CoursesController::class, 'getCourses']);
+Route::post('/courses', [CoursesController::class, 'createCourse']);
