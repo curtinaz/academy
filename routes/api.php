c391fab3f7e36ada83acfbc9a71e734b03f3a42d
Cristian Curtinaz <curtinazif@gmail.com>
2022-11-01 08:03:08 -0300

    New Courses and Modules routes added

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\CoursesController;
+use App\Http\Controllers\ModulesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/courses',[CoursesController::class, 'getCourses']);
 Route::post('/courses', [CoursesController::class, 'createCourse']);
+Route::get('/courses/{course_id}', [CoursesController::class, 'show']);
 Route::patch('/courses/{course_id}', [CoursesController::class, 'updateCourse']);
 Route::delete('/courses/{course_id}', [CoursesController::class, 'deleteCourse']);
+
+
+Route::get('/modules/{module_id}', [ModulesController::class, 'show']);
